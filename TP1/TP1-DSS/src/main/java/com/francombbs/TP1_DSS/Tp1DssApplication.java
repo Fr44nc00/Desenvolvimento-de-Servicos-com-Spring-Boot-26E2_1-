19d46abaae5d60825a72ba92086c3e7ebc440fe7
Mateus Franco <mateusfsoares08@gmail.com>
2026-05-12 20:21:35 -0300

    Adicionando os arquivos do projeto

---
+++ TP1/TP1-DSS/src/main/java/com/francombbs/TP1_DSS/Tp1DssApplication.java
@@ -0,0 +1,13 @@
+package com.francombbs.TP1_DSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1DssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1DssApplication.class, args);
+	}
+
+}
---
+++ TP1/TP1-DSS/src/test/java/com/francombbs/TP1_DSS/Tp1DssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francombbs.TP1_DSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1DssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
