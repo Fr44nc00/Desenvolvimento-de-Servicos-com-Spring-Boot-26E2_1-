1ee41d651de8ba94666d7ff95e640635f3a719bf
Mateus Franco <mateusfsoares08@gmail.com>
2026-05-24 19:27:44 -0300

    Criando a pasta do TP2

---
+++ TP2/tp2-dss-cli/src/main/java/com/francombss/tp2_dss/Tp2DssApplication.java
@@ -0,0 +1,13 @@
+package com.francombss.tp2_dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2DssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2DssApplication.class, args);
+	}
+
+}
---
+++ TP2/tp2-dss-cli/src/test/java/com/francombss/tp2_dss/Tp2DssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francombss.tp2_dss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2DssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
